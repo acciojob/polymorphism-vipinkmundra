c54ae452f20e77640c2b59ac1ca24800cf7af13c
vipin <vipinkmundra@gmail.com>
2023-01-12 22:11:12 +0530

    solution 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static void Main(String[] args){
+        Product p = new Product();
+        int ans1 = p.product(5,6);
+        int ans2 = p.product(5,6,7);
+        double ans3 = p.product(2.5,7.1);
+    }
+}
 
+class Product{
+    public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double product(double x,double y){
+        return x*y*z;
+    }
 }
