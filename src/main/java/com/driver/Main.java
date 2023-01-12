9840e2d720ca44cd3cfc0fec9a858504046ba315
vipin <vipinkmundra@gmail.com>
2023-01-12 23:11:27 +0530

    solution3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,19 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
     public static void Main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
         Product p = new Product();
-        int ans1 = p.product(5,6);
-        int ans2 = p.product(5,6,7);
-        double ans3 = p.product(2.5,7.1);
+        int ans1 = p.product(x,y);
+        int ans2 = p.product(x,y,z);
+        double ans3 = p.product(a,b);
         System.out.println(ans1);
         System.out.println(ans2);
         System.out.println(ans3);
