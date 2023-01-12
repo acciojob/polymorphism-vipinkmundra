e0508c032ab7affda0e646695884e46b4759de77
vipin <vipinkmundra@gmail.com>
2023-01-12 22:53:46 +0530

    "solution2"

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,17 +6,20 @@ public class Main {
         int ans1 = p.product(5,6);
         int ans2 = p.product(5,6,7);
         double ans3 = p.product(2.5,7.1);
+        System.out.println(ans1);
+        System.out.println(ans2);
+        System.out.println(ans3);
     }
 }
 
 class Product{
-    public int product(int x,int y){
+    public static int product(int x,int y){
         return x*y;
     }
-    public int product(int x,int y,int z){
+    public static int product(int x,int y,int z){
         return x*y*z;
     }
-    public double product(double x,double y){
-        return x*y*z;
+    public static double product(double x,double y){
+        return x*y;
     }
 }
